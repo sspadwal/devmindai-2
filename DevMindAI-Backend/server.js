12262d5c03a4bdcb7a71adcf6ec5d0d3aa942c33
sspadwal <sspadwal1002@gmail.com>
2026-07-24 22:14:19 +0530

    fixed the frontend url

--- DevMindAI-Backend/server.js
+++ DevMindAI-Backend/server.js
@@ -3,7 +3,7 @@ import "dotenv/config"
 import {aiRoutes} from "./src/models/ai/ai.routes.js"
 import cors from "cors"
 const corsOptions={
-    origin:"http://localhost:5173",
+    origin:"https://devmindai.vercel.app",
     methods:['GET','POST','PUT','DELETE'],
     allowedHeaders:['Content-Type','Authorization','X-User-ID'],
     credentials:true
